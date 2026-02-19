dcf5787905616ec0c28ea08209f88c73c72772bf
Manguyajuma <manguyajuma282@gmail.com>
2026-02-18 19:27:14 -0800

    new auth

---
+++ lib/services/auth/auth.dart
@@ -0,0 +1,27 @@
+class AuthGate extends StatefulWidget {
+  const AuthGate({super.key});
+
+  @override
+  State<AuthGate> createState() => _AuthGateState();
+}
+
+class _AuthGateState extends State<AuthGate> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          // user is logged in
+          if (snapshot.hasData) {
+            return HomePage();
+          }
+          // user is not logged in
+          else {
+            return const LoginOrRegister();
+          }
+        },
+      ),
+    );
+  }
+}
